95d9fb63f43cb9ab47f46f641310939389dab023
Alliance-D <a.dushimezi@alustudent.com>
2025-05-23 10:12:28 +0000

    Linear algebra assignment

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+def add_matrices2D(mat1, mat2):
+    if len(mat1) != len(mat2) or any(len(r1) != len(r2) for r1, r2 in zip(mat1, mat2)):
+        return None
+    return [[c1 + c2 for c1, c2 in zip(r1, r2)] for r1, r2 in zip(mat1, mat2)]
