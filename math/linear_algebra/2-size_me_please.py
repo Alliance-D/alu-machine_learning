641d56b2c33c299c627fff115c298c062eb419f7
Alliance-D <a.dushimezi@alustudent.com>
2025-05-23 18:23:40 +0000

    documenting for no2 and fixing no of lines

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
-#function that calculates the shape of a mtrix
 def matrix_shape(matrix):
+"""
+    Calculates the shape of a matrix.
+
+    Args:
+        matrix (list): A nested list (matrix) where all elements in the same dimension
+                       are of the same type and shape.
+
+    Returns:
+        list: A list of integers representing the shape of the matrix.
+    """
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
