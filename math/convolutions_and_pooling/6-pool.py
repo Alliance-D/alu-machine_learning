84461f2b6bb1d35d6102b521a0556494e1b1221c
Alliance-D <a.dushimezi@alustudent.com>
2025-07-08 04:47:53 -0700

    Bayesian probability

--- math/convolutions_and_pooling/6-pool.py
+++ math/convolutions_and_pooling/6-pool.py
@@ -1,38 +1,37 @@
 #!/usr/bin/env python3
+"""Module that performs pooling on images."""
+
+
 import numpy as np
 
-def pool(images, kernel_shape, stride, mode='max'):
-    """Performs pooling on images.
 
-    Args:
-        images: numpy.ndarray of shape (m, h, w, c)
-        kernel_shape: tuple of (kh, kw)
-        stride: tuple of (sh, sw)
-        mode: 'max' or 'avg'
+def pool(images, kernel_shape, stride, mode='max'):
+    """
+    Performs pooling on images.
+
+    Parameters:
+    - images: np.ndarray of shape (m, h, w, c)
+    - kernel_shape: tuple of (kh, kw)
+    - stride: tuple of (sh, sw)
+    - mode: 'max' or 'avg'
 
     Returns:
-        numpy.ndarray of pooled images
+    - np.ndarray: pooled images
     """
     m, h, w, c = images.shape
     kh, kw = kernel_shape
     sh, sw = stride
 
-    # Calculate the output dimensions
     out_h = (h - kh) // sh + 1
     out_w = (w - kw) // sw + 1
+    pooled = np.zeros((m, out_h, out_w, c))
 
-    # Initialize output
-    output = np.zeros((m, out_h, out_w, c))
-
-    # Loop through output height and width
     for i in range(out_h):
         for j in range(out_w):
-            # Extract slice for pooling
-            slice_img = images[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :]
-
+            region = images[:, i * sh:i * sh + kh, j * sw:j * sw + kw, :]
             if mode == 'max':
-                output[:, i, j, :] = np.max(slice_img, axis=(1, 2))
+                pooled[:, i, j, :] = np.max(region, axis=(1, 2))
             elif mode == 'avg':
-                output[:, i, j, :] = np.mean(slice_img, axis=(1, 2))
+                pooled[:, i, j, :] = np.mean(region, axis=(1, 2))
 
-    return output
+    return pooled
