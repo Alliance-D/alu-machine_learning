a9a52ef9ace68098f51b4c36e014b5359bd7b63b
Alliance-D <a.dushimezi@alustudent.com>
2025-07-08 03:22:38 -0700

    Bayesian probability

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,20 +1,36 @@
 #!/usr/bin/env python3
+"""
+Performs a valid convolution on grayscale images.
+"""
+
 import numpy as np
 
+
 def convolve_grayscale_valid(images, kernel):
-    """Performs a valid convolution on grayscale images."""
+    """
+    Performs a valid convolution on grayscale images.
+    
+    Parameters:
+    - images: numpy.ndarray of shape (m, h, w)
+      Multiple grayscale images
+    - kernel: numpy.ndarray of shape (kh, kw)
+      Kernel for the convolution
+
+    Returns:
+    - numpy.ndarray containing the convolved images
+    """
     m, h, w = images.shape
     kh, kw = kernel.shape
     output_h = h - kh + 1
     output_w = w - kw + 1
 
-    # Initialize the output
     output = np.zeros((m, output_h, output_w))
 
     for i in range(output_h):
         for j in range(output_w):
-            # Extract the region of interest and apply the kernel
-            region = images[:, i:i+kh, j:j+kw]
-            output[:, i, j] = np.sum(region * kernel, axis=(1, 2))
-    
+            output[:, i, j] = np.sum(
+                images[:, i:i+kh, j:j+kw] * kernel,
+                axis=(1, 2)
+            )
+
     return output
