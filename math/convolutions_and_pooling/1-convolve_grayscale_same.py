593edc3e1afe0cbe70a491de5a6e471daa0a357b
Alliance-D <a.dushimezi@alustudent.com>
2025-07-08 03:38:10 -0700

    Bayesian probability

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -24,8 +24,11 @@ convolution
     pad_h = kh // 2
     pad_w = kw // 2
 
-    padded = np.pad(images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w)), mode=
-        'constant')
+    padded = np.pad(
+        images,
+        ((0, 0), (pad_h, pad_h), (pad_w, pad_w)),
+        mode='constant'
+    )
     output = np.zeros((m, h, w))
 
     for i in range(h):
