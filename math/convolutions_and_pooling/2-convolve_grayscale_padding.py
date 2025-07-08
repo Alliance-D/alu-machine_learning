636d90644b0e9a89802d953a09b06d637deec462
Alliance-D <a.dushimezi@alustudent.com>
2025-07-08 03:45:35 -0700

    Bayesian probability

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -1,32 +1,36 @@
 #!/usr/bin/env python3
+"""Module that performs a convolution on grayscale images with custom
+padding."""
+
+
 import numpy as np
+
 
 def convolve_grayscale_padding(images, kernel, padding):
     """
     Performs a convolution on grayscale images with custom padding.
 
     Parameters:
-    - images: numpy.ndarray of shape (m, h, w) containing grayscale images
-    - kernel: numpy.ndarray of shape (kh, kw) with the kernel for convolution
-    - padding: tuple of (ph, pw), padding for height and width
+    - images (numpy.ndarray): shape (m, h, w), multiple grayscale images
+    - kernel (numpy.ndarray): shape (kh, kw), the kernel for the convolution
+    - padding (tuple): (ph, pw), the padding for the height and width
 
     Returns:
-    - convolved images as a numpy.ndarray
+    - numpy.ndarray: convolved images of shape (m, h + 2*ph - kh + 1, w + 2*pw
+- kw + 1)
     """
     m, h, w = images.shape
     kh, kw = kernel.shape
     ph, pw = padding
 
-    padded_images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), mode='constant')
+    padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), mode='constant')
+    out_h = h + 2 * ph - kh + 1
+    out_w = w + 2 * pw - kw + 1
+    output = np.zeros((m, out_h, out_w))
 
-    new_h = h + 2 * ph - kh + 1
-    new_w = w + 2 * pw - kw + 1
-
-    output = np.zeros((m, new_h, new_w))
-
-    for i in range(new_h):
-        for j in range(new_w):
-            image_slice = padded_images[:, i:i+kh, j:j+kw]
-            output[:, i, j] = np.sum(image_slice * kernel, axis=(1, 2))
+    for i in range(out_h):
+        for j in range(out_w):
+            region = padded[:, i:i + kh, j:j + kw]
+            output[:, i, j] = np.sum(region * kernel, axis=(1, 2))
 
     return output
