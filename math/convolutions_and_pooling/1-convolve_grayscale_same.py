a5e68f83301a410c2e5a7000190a75b3fc0abf33
Alliance-D <a.dushimezi@alustudent.com>
2025-07-08 03:32:15 -0700

    Bayesian probability

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -1,28 +1,35 @@
 #!/usr/bin/env python3
+"""Module that performs a same convolution on grayscale images."""
+
+
 import numpy as np
 
+
 def convolve_grayscale_same(images, kernel):
-    """Performs a same convolution on grayscale images"""
+    """
+    Performs a same convolution on grayscale images.
+
+    Parameters:
+    - images (numpy.ndarray): shape (m, h, w) containing multiple grayscale
+images
+    - kernel (numpy.ndarray): shape (kh, kw) containing the kernel for the
+convolution
+
+    Returns:
+    - numpy.ndarray: shape (m, h, w) containing the convolved images
+    """
     m, h, w = images.shape
     kh, kw = kernel.shape
 
-    # Calculate padding
     pad_h = kh // 2
     pad_w = kw // 2
 
-    # Pad images with 0s on height and width
-    padded = np.pad(images,
-                    pad_width=((0, 0), (pad_h, pad_h), (pad_w, pad_w)),
-                    mode='constant')
-
-    # Initialize output
+    padded = np.pad(images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w)), mode='constant')
     output = np.zeros((m, h, w))
 
-    # Perform convolution with two for-loops
     for i in range(h):
         for j in range(w):
-            output[:, i, j] = np.sum(
-                padded[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2)
-            )
+            region = padded[:, i:i + kh, j:j + kw]
+            output[:, i, j] = np.sum(region * kernel, axis=(1, 2))
 
     return output
