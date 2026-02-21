b06a977ddb83c1025df5afec5d6821bf103c2b28
Alliance-D <a.dushimezi@alustudent.com>
2026-02-21 06:34:59 -0800

    fix

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -14,7 +14,6 @@ def pca(X, var=0.95):
         W: numpy.ndarray of shape (d, nd) weights matrix
     """
     _, s, Vt = np.linalg.svd(X, full_matrices=False)
-    total = np.sum(s ** 2)
-    cumvar = np.cumsum(s ** 2) / total
+    cumvar = np.cumsum(s ** 2) / np.sum(s ** 2)
     nd = np.searchsorted(cumvar, var) + 1
     return Vt[:nd].T
