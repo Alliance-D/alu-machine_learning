f1399b08c7720b8624282f7f4c604cf616057102
Alliance-D <a.dushimezi@alustudent.com>
2026-02-21 05:34:53 -0800

    dimensionality_reduction

---
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Module for performing PCA on a dataset."""
+import numpy as np
+
+
+def pca(X, var=0.95):
+    """Perform PCA on a dataset maintaining a fraction of variance.
+
+    Args:
+        X: numpy.ndarray of shape (n, d) with zero-mean data
+        var: fraction of variance to maintain
+
+    Returns:
+        W: numpy.ndarray of shape (d, nd) weights matrix
+    """
+    _, s, Vt = np.linalg.svd(X)
+    cumvar = np.cumsum(s ** 2) / np.sum(s ** 2)
+    nd = np.argmax(cumvar >= var) + 1
+    return Vt[:nd].T
